db41001f17ec9f989ce263a092b4c80874b25fb4
Nguyen Huu Loc <locn562836@gmail.com>
2025-10-04 17:09:25 +0700

    Add security configuration for OAuth2 and gRPC in application.yml; refactor NotificationServiceImpl to use UserGrpcClient

--- src/main/java/vn/vinaacademy/notification/NotificationApplication.java
+++ src/main/java/vn/vinaacademy/notification/NotificationApplication.java
@@ -4,16 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import vn.vinaacademy.kafka.KafkaConsumerConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {
-        "vn.vinaacademy.notification",
-        "vn.vinaacademy.common"
-})
 @EnableFeignClients
 public class NotificationApplication {
 
